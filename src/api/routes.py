014995a570669086607fd732e0a25030363eba3d
Óscar Sánchez <oscarsanchezmeza97@gmail.com>
2021-03-16 13:18:11 +0000

    Fixing the url from the backend

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_cors import CORS
 
 api = Blueprint('api', __name__)
 
